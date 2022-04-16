d70773196a868e81ba7ee7b8cd9a2c322ac0d6d8
Lichen <297324608@qq.com>
2022-04-16 16:05:53 +0800

    创建r2dbc connection pool

--- server/server-common/src/main/java/com/lichen/github/website/server/common/codec/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- *
- * 这个 package主要存放一些 {@link dev.miku.r2dbc.mysql.codec.Codec} 类。
- *
- * 主要用来做 mysql的类型和JavaType类型的转换。
- *
- */
-package com.lichen.github.website.server.common.codec;
--- server/server-common/src/main/java/com/lichen/github/website/server/common/dal/model/PersistentInstanceImpl.java
+++ server/server-common/src/main/java/com/lichen/github/website/server/common/dal/model/PersistentInstanceImpl.java
@@ -1,4 +1,4 @@
-package com.lichen.github.website.server.common.dal;
+package com.lichen.github.website.server.common.dal.model;
 
 import lombok.Getter;
 import lombok.Setter;
